04c8ca286b8f14efa08e6fe3f314196b734f4db1
robindegroot <robindegroot.rdg@gmail.com>
2018-01-15 14:15:36 +0100

    added loop
    added a draw loop

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
 var tree;
 var max_dist = 100;
 var min_dist = 30;
-
+//create canvas and tree class
 function setup() {
   createCanvas(900, 900);
   tree = new Tree();
 }
-
+// draw tree
 function draw() {
   background(47);
   tree.show();
   tree.grow();
 }
-function mousePressed(){
-  redraw();
-}
+setTimeout(function()
+{
+  location.reload();
+}, 6000);
